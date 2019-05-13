1177c1a7712cf1276399d84c2cd388954c79effd
vmstr <marcroesner@googlemail.com>
2019-05-13 15:57:49 +0200

    Update uuidGenerator.js

--- uuidGenerator.js
+++ uuidGenerator.js
@@ -5,12 +5,14 @@
   broofa (2018): https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript 
 */ 
 
-var UUID = function() {};
+var UUIDGenerator = function() {};
 
-UUID.prototype.generate = function () {
+UUIDGenerator.prototype.generate = function () {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, 
             v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
+
+var UUID = new UUIDGenerator(); 
