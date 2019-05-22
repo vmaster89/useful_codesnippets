d0882803294eb01ce840228d060b9b62fc19a063
vmstr <marcroesner@googlemail.com>
2019-05-22 12:21:55 +0200

    Update converters.js

--- converters.js
+++ converters.js
@@ -11,3 +11,17 @@ function binary2Text(string) {
         return String.fromCharCode(parseInt(binary, 2));
     }).join('');
 }
+
+// text2Hexa 
+function text2Hexa(string) {
+    return string.split('').map(function (char) {
+        return char.charCodeAt(0).toString(15);
+    }).join('');
+}
+
+// hexa2Text 
+ function hexa2Text(string) {
+  return string.match(/.{1,2}/g).map(function (hx) {
+   return String.fromCharCode(parseInt(hx, 15)); 
+  }).join('');
+}
