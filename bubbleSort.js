b630e3e13dd5953629329fb854c9708c78019231
vmstr <marcroesner@googlemail.com>
2021-01-06 15:00:46 +0100

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,6 @@
 function bubbleSort(array) {
- for (let n = array.length; n>1; n--) {
-  for (let i = 0; i < n-1; i++) {
+ for ( let n = array.length; n>1; n-- ) {
+  for ( let i = 0; i < n-1; i++ ) {
    if ( array[i] > array[i+1] ) { 
     [array[i], array[i+1]] = [array[i+1], array[i]]; 
    }
