232e2721b14d589cfddf6edcf30467b8bd270e80
vmstr <marcroesner@googlemail.com>
2021-01-06 15:01:26 +0100

    Delete bubbleSort.js

--- bubbleSort.js
+++
@@ -1,9 +0,0 @@
-function bubbleSort(array) {
- for ( let n = array.length; n>1; n-- ) {
-  for ( let i = 0; i < n-1; i++ ) {
-   if ( array[i] > array[i+1] ) { 
-    [array[i], array[i+1]] = [array[i+1], array[i]]; 
-   }
-  }
- }
-}
