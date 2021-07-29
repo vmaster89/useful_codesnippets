9367a71a221841a2dbf84bedb711da70cf6f576b
vmstr <marcroesner@googlemail.com>
2021-07-29 21:44:01 +0200

    Update js-prototype-singleton-list-instances.js

--- js-prototype-singleton-list-instances.js
+++ js-prototype-singleton-list-instances.js
@@ -1,13 +1,4 @@
-var UUIDGenerator = function() {};
-
-UUIDGenerator.prototype.generate = function () {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, 
-            v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
+// Imported 
 var UUID = new UUIDGenerator(); 
 
 const BackendService = {
